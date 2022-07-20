72e2f9565c5f1375385b9f67b06ec1deea8fb6ad
NKC <khacchang128@gmail.com>
2022-07-20 23:38:59 +0700

    first commit

---
+++ src/request.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://uniqlo-nkc.herokuapp.com/api";
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+const token = localStorage.getItem("token");
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    authorization: `Bearer ${token}`,
+  },
+});
